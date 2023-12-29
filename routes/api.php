adca45e71629921aff7c14392a8bbdb247632b49
root <root@ip-172-31-35-143.us-east-2.compute.internal>
2023-12-29 22:46:32 +0000

    test

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('get_invoice',[\App\Http\Controllers\ApiInvoice::class, 'index'])->name('get_invoice');
 Route::post('cancel_invoice',[\App\Http\Controllers\ApiInvoice::class, 'CancelInvoice'])->name('cancel_invoice');
 Route::post('query_invoice',[\App\Http\Controllers\ApiInvoice::class, 'QueryInvoice'])->name('query_invoice');
-<<<<<<< HEAD
 Route::post('query_invoice_xml',[\App\Http\Controllers\ApiInvoice::class, 'QueryInvoiceXML'])->name('query_invoice_xml');
 Route::post('query_invoice_qr',[\App\Http\Controllers\ApiInvoice::class, 'QueryInvoiceQR'])->name('query_invoice_qr');
-=======
-Route::post('query_invoice_canceled',[\App\Http\Controllers\ApiInvoice::class, 'QueryInvoiceCanceled'])->name('query_invoice_canceled');
->>>>>>> 731a64b5a8f3ced84c0b3d2751605c77bca948c7
